4ac369a200172150072b63661ff405c04cbf3692
mursal-rash <rashmurs24@gmail.com>
2024-09-26 14:12:15 +0300

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+using std::cout, std::endl;
+struct Book
+{
+char title[100];
+int pages;
+float  price;
+};
+bool isExpensive(const Book& a ){
+   if (a.price>1000)
+    return 1;
+    else return 0;
+   
+
+}
+int main()
+{
+struct Book a;
+std::cin>>a.title;
+std::cin>>a.pages;
+std::cin>>a.price;
+std::cout<<isExpensive(a)<<std::endl;
+}
